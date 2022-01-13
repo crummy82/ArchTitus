18bb9a340e0cf1c8199128be2e673898ecb5b4d3
crummy82 <bradleycrum@gmail.com>
2022-01-13 12:32:39 -0500

    Removed kitty files. Modified KDE ricing

--- kderice-restore.sh
+++ kderice-restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PATH=$PATH:~/.local/bin
-cp -r $HOME/$SCRIPTHOME/dotfiles/* $HOME/.config/
+#export PATH=$PATH:~/.local/bin
+#cp -r $HOME/$SCRIPTHOME/dotfiles/* $HOME/.config/
 pip install konsave
 konsave -i $HOME/$SCRIPTHOME/kde.knsv
 sleep 1
